e4b2411825e3874a9fe518c40b37a560114217c8
RidhiJaisingh <ridhi.21bce8341@vitapstudent.ac.in>
2023-11-03 17:37:15 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Wordcount from "./components/Wordcount";
+function App() {
+  return (
+    <div className="App">
+      <Wordcount />
+    </div>
+  );
+}
+
+export default App;
